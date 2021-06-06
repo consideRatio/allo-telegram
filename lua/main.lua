e4aef826032e1533f691ba43491ede95c865e7f6
Erik Sundell <erik.i.sundell@gmail.com>
2021-06-06 02:04:53 +0200

    Initial commit after getting started guide

--- lua/main.lua
+++ lua/main.lua
@@ -9,53 +9,141 @@ local client = Client(
 -- your app.
 local app = App(client)
 
+
+
 -- Assets are files (images, glb models, videos, sounds, etc...) that you want to use
 -- in your app. They need to be published so that user's headsets can download them
 -- before you can use them. We make `assets` global so you can use it throughout your app.
 assets = {
     quit = ui.Asset.File("images/quit.png"),
+    checkmark = ui.Asset.File("images/checkmark.png"),
 }
 app.assetManager:add(assets)
 
--- mainView is the main UI for your app. Set it up before connecting.
--- 0, 1.2, -2 means: put the app centered horizontally; 1.2 meters up from the floor; and 2 meters into the room, depth-wise
--- 1, 0.5, 0.01 means 1 meter wide, 0.5 meters tall, and 1 cm deep.
--- It's a surface, so the depth should be close to zero.
-local mainView = ui.Surface(ui.Bounds(0, 1.2, -2,   1, 0.5, 0.01))
 
--- Make it so that the grab button or right mouse button moves lets user move the view.
--- Instead of making mainView grabbable, you could also create a ui.GrabHandle and add it
--- as a subview, sort of like a title bar of a desktop window.
-mainView.grabbable = true
 
--- Create a regular ol' button. Make it 2 dm wide and high, and 1dm deep.
--- The position refers to the center of the button, so it needs to be moved
--- 5cm out of the mainView so that the button lies on top of the surface,
--- instead of embedded inside of it.
-local button = ui.Button(ui.Bounds(0.0, 0.05, 0.05,   0.2, 0.2, 0.1))
-mainView:addSubview(button)
 
--- When user presses the button, print "hello" to the local terminal.
--- Here's where you can make the button do more fun stuff :)
-button.onActivated = function()
-    print("Hello!")
+class.TodosView(ui.Surface)
+function TodosView:_init(bounds)
+    self:super(bounds)
+    self.grabbable = true
+
+    self.quitButton = self:addSubview(ui.Button(ui.Bounds{
+        size=ui.Size(0.12,0.12,0.05)
+    }))
+    self.quitButton:setDefaultTexture(assets.quit)
+    self.quitButton.onActivated = function()
+        app:quit()
+    end
+
+    self.addButton = self:addSubview(ui.Button(ui.Bounds{
+        size=ui.Size(bounds.size.width*0.8,0.1,0.05)
+    }))
+    self.addButton.label:setText("Add todo")
+    self.addButton.onActivated = function(hand)
+        self:showNewTodoPopup(hand)
+    end
+
+    self.todoViews = {}
+    self:layout()
 end
 
--- It's nice to provide a way to quit the app, too.
--- Here's also an alternative syntax for setting the size and position of something.
-local quitButton = ui.Button(ui.Bounds{size=ui.Size(0.12,0.12,0.05)}:move( 0.52,0.25,0.025))
--- Use our quit texture file as the image for this button.
-quitButton:setDefaultTexture(assets.quit)
-quitButton.onActivated = function()
-    app:quit()
-end
-mainView:addSubview(quitButton)
 
--- Tell the app that mainView is the primary UI for this app
-app.mainView = mainView
+
+function TodosView:layout()
+    local height = #self.todoViews * 0.13 + 0.25
+
+    local pen = ui.Bounds{
+        size=self.addButton.bounds.size:copy(),
+        pose=ui.Pose(0, -height/2, self.addButton.bounds.size.depth/2)
+    }
+    pen:move(0, 0.07, 0)
+    self.addButton:setBounds(pen:copy())
+    pen:move(0, 0.15, 0)
+    for i, v in ipairs(self.todoViews) do
+        v:setBounds(pen:copy())
+        pen:move(0, 0.13, 0)
+    end
+
+    self.quitButton.bounds:moveToOrigin():move( 0.52, height/2, 0.025)
+    self.quitButton:setBounds()
+
+    self.bounds.size.height = height
+    self:setBounds()
+end
+
+
+
+
+function TodosView:showNewTodoPopup(hand)
+    local popup = ui.Surface(ui.Bounds{size=ui.Size(1,0.5,0.05)})
+
+    local input = popup:addSubview(ui.TextField{
+        bounds = ui.Bounds{size=ui.Size(0.8,0.1,0.05)}:move(0, 0.15, 0.025)
+    })
+    local done = function()
+        self:addTodo(input.label.text)
+        popup:removeFromSuperview()
+    end
+    input.onReturn = function()
+        done()
+        return false
+    end
+    input:askToFocus(hand)
+
+    local addButton = popup:addSubview(ui.Button(ui.Bounds{size=ui.Size(popup.bounds.size.width*0.8,0.1,0.05)}))
+    addButton.bounds:move(0, 0, 0.025)
+    addButton.label:setText("Add")
+    addButton.onActivated = done
+
+    local cancelButton = popup:addSubview(ui.Button(ui.Bounds{size=ui.Size(popup.bounds.size.width*0.8,0.1,0.05)}))
+    cancelButton:setColor({0.4, 0.4, 0.3, 1.0})
+    cancelButton.bounds:move(0, -0.15, 0.025)
+    cancelButton.label:setText("Cancel")
+    cancelButton.onActivated = function()
+        popup:removeFromSuperview()
+    end
+
+    app:openPopupNearHand(popup, hand)
+end
+
+function TodosView:addTodo(text)
+    local todoView = ui.View(ui.Bounds{size=ui.Size(self.bounds.size.width*0.8,0.1,0.05)})
+
+    local checkButton = todoView:addSubview(ui.Button(ui.Bounds{size=ui.Size(0.1, 0.1, 0.05)}))
+    checkButton.bounds:move(-self.bounds.size.width/2 + checkButton.bounds.size.width, 0, 0)
+    checkButton:setDefaultTexture(assets.checkmark)
+    checkButton.onActivated = function()
+        self:removeTodo(todoView)
+    end
+
+    local label = todoView:addSubview(ui.Label{
+        bounds= todoView.bounds:copy():inset(0.1, 0.05, 0):move(0.05, 0,0),
+        color= {0,0,0, 1},
+        halign= "left",
+        text= text
+    })
+
+    table.insert(self.todoViews, todoView)
+    self:layout()
+    self:addSubview(todoView)
+end
+
+function TodosView:removeTodo(todoView)
+    local index = tablex.find(self.todoViews, todoView)
+    table.remove(self.todoViews, index)
+    todoView:removeFromSuperview()
+    self:layout()
+end
+
+
 
 -- Connect to the designated remote Place server
 app:connect()
+-- hand over runtime to the app! App will now run forever,
+-- or until the app is shut down (ctrl-C or exit button pressed).
+app:run()
+
 -- hand over runtime to the app! App will now run forever,
 -- or until the app is shut down (ctrl-C or exit button pressed).
 app:run()
